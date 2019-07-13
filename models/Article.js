191e954c283e4af26d5265a6d8fbb4016d8dd3dd
lynne.kwik@gmail.com <lynne.kwik@gmail.com>
2019-07-13 16:54:20 -0700

    added article and comment models

--- models/Article.js
+++ models/Article.js
@@ -2,8 +2,18 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 varArticleSchema = new Schema({
-	name:
-
+	title: {
+		type: String,
+		required: true
+	},
+	link: {
+		type: String,
+		required: true
+	},
+	comment: {
+		type: Schema.Types.ObjectId,
+		ref: "Comment"
+	}
 });
 //This is my model from the above schema, using mongoose's model method//
 var Article = mongoose.model("Article", ArticleSchema);
